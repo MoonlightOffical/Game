3633b454d4e1b19a9e8ecaa51a6d637e419f33d7
Moonlight Hub <presidentialrealmsstudio@gmail.com>
2025-05-01 07:51:16 +0700

    Create Gamelist.lua

---
+++ GameList/Gamelist.lua
@@ -0,0 +1,15 @@
+local Games = {
+  [2753915549] = "nil", -- Blox Fruit Sea 1
+  [4442272183] = "nil", -- Blox Fruit Sea 2
+  [7449423635] = "nil", -- Blox Fruit Sea 3
+  [16732694052] = "nil", -- Fisch
+  [72907489978215] = "nil", -- 2 Sea Fisch
+  [116495829188952] = "nil", -- Dead Rails
+  [70876832253163] = "", -- Dead Rails
+  [125723653259639] = "nil", -- Drill Digging Simulator
+  [128336380114944] = "nil", -- Arise Crossover | Dungeon
+  [116614712661486] = "nil", -- Arise Crossover | AFK Reward
+  [85896571713843] = "nil" -- Bubble Gum Simulator INFINITY
+}
+
+return Games

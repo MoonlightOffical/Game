3fdaf2706aa4559dbc418dcc1ace82d4d1e62c8a
Moonlight Hub <presidentialrealmsstudio@gmail.com>
2025-07-05 13:40:13 +0700

    Update Gamelist.lua

--- GameList/Gamelist.lua
+++ GameList/Gamelist.lua
@@ -1,8 +1,8 @@
 local Games = {
   [122678592501168] = "https://raw.githubusercontent.com/MoonlightOffical/Game/refs/heads/main/Beaks.lua", -- Beaks
   [81440632616906] = "https://raw.githubusercontent.com/MoonlightOffical/Game/refs/heads/main/DTEC.lua", -- DTEC
-  [99791605069929] = "https://raw.githubusercontent.com/MoonlightOffical/Game/refs/heads/main/BIB.lua" -- BIB
-  [111989938562194] = "https://raw.githubusercontent.com/MoonlightOffical/Game/refs/heads/main/BrainrotEvo.lua" -- Brain Evo
+  [99791605069929] = "https://raw.githubusercontent.com/MoonlightOffical/Game/refs/heads/main/BIB.lua", -- BIB
+  [111989938562194] = "https://raw.githubusercontent.com/MoonlightOffical/Game/refs/heads/main/BrainrotEvo.lua", -- Brain Evo
   [126884695634066] = "https://raw.githubusercontent.com/MoonlightOffical/Game/refs/heads/main/GrowAGarden.lua" -- GROW A GARDEN
 }
 

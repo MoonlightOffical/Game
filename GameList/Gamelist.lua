31e2f656ff8c4e05eb567635506532f8dcd73c91
Moonlight Hub <presidentialrealmsstudio@gmail.com>
2025-07-05 13:33:54 +0700

    Update Gamelist.lua

--- GameList/Gamelist.lua
+++ GameList/Gamelist.lua
@@ -1,15 +1,4 @@
 local Games = {
-  [2753915549] = "nil", -- Blox Fruit Sea 1
-  [4442272183] = "nil", -- Blox Fruit Sea 2
-  [7449423635] = "nil", -- Blox Fruit Sea 3
-  [16732694052] = "nil", -- Fisch
-  [72907489978215] = "nil", -- 2 Sea Fisch
-  [116495829188952] = "nil", -- Dead Rails
-  [70876832253163] = "nil", -- Dead Rails
-  [125723653259639] = "nil", -- Drill Digging Simulator
-  [128336380114944] = "nil", -- Arise Crossover | Dungeon
-  [116614712661486] = "nil", -- Arise Crossover | AFK Reward
-  [85896571713843] = "nil", -- Bubble Gum Simulator INFINITY
   [122678592501168] = "https://raw.githubusercontent.com/MoonlightOffical/Game/refs/heads/main/Beaks.lua", -- Beaks
   [81440632616906] = "https://raw.githubusercontent.com/MoonlightOffical/Game/refs/heads/main/DTEC.lua", -- DTEC
   [99791605069929] = "https://raw.githubusercontent.com/MoonlightOffical/Game/refs/heads/main/BIB.lua" -- BIB

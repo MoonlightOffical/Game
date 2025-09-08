31fdb4032827d0c0357baa41793dcb0cf5180635
Moonlight Hub <presidentialrealmsstudio@gmail.com>
2025-09-08 20:55:39 +0700

    Update Gamelist.lua

--- GameList/Gamelist.lua
+++ GameList/Gamelist.lua
@@ -4,6 +4,8 @@ local Games = {
   [99791605069929] = "https://raw.githubusercontent.com/MoonlightOffical/Game/refs/heads/main/BIB.lua", -- BIB
   [111989938562194] = "https://raw.githubusercontent.com/MoonlightOffical/Game/refs/heads/main/BrainrotEvo.lua", -- Brain Evo
   [126884695634066] = "https://raw.githubusercontent.com/MoonlightOffical/Game/refs/heads/main/GrowAGarden.lua" -- GROW A GARDEN
+  [109983668079237] = "https://api.junkie-development.de/api/v1/luascripts/public/a7d8a3b32870e220aab4c001e690dffd15cbef8ea6dde4d87b7fdaad21eee0f7/download" -- SAB [Old Server]
+  [000000000000001] = "https://api.junkie-development.de/api/v1/luascripts/public/a7d8a3b32870e220aab4c001e690dffd15cbef8ea6dde4d87b7fdaad21eee0f7/download" -- SAB [NEW PLAYER SERVER]
 }
 
 return Games
